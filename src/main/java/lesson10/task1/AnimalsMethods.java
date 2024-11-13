b3052b5fd29eb04e50acfe0965c06cde88aa21e2
Egor <egorgoldenberg@MacBook-Air-Egor.local>
2024-11-13 14:04:06 +0300

    lesson6

---
+++ src/main/java/lesson10/task1/AnimalsMethods.java
@@ -0,0 +1,6 @@
+package lesson10.task1;
+
+public interface AnimalsMethods {
+    void run(int distance);
+    void swim(int distance);
+}
---
+++ src/main/java/lesson10/task2/ShapeInterface.java
@@ -0,0 +1,9 @@
+package lesson10.task2;
+
+public interface ShapeInterface {
+    public double calculationPerimeter();
+
+    public double calculationArea();
+
+    public void displayInfo();
+}
