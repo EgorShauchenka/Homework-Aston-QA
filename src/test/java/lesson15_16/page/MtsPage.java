c572fb540aac9dc2139de9dbc6678a124fdf8290
Egor <egorgoldenberg@MacBook-Air-Egor.local>
2024-11-30 23:06:36 +0300

    lesson15 selenium

--- src/test/java/lesson15_16/page/MtsPage.java
+++ src/test/java/lesson15_16/page/MtsPage.java
@@ -8,10 +8,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
 import java.time.Duration;
-import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -92,88 +89,7 @@ public class MtsPage {
         return element.getText();
     }
 
-    public MtsPage clickButtonHeader() {
-        buttonHeader.click();
-        return this;
-    }
-
-    public MtsPage selectService(String serviceName) {
-        WebElement selectedOption = driver.findElement(By.xpath("//option[@value='" + serviceName + "']"));
-        selectedOption.click();
-        return this;
-    }
-
-    public MtsPage checkFields(String serviceName, String... expectedLabels) {
-        String formId = driver.findElement(By.xpath("//option[@value='" + serviceName + "']"))
-                .getAttribute("data-open");
-
-        List<WebElement> inputFields = driver.findElements(By.cssSelector("#" + formId + " input[type='text']"));
-
-        for (WebElement field : inputFields) {
-            String label = field.getAttribute("placeholder");
-            assertTrue(Arrays.asList(expectedLabels).contains(label));
-        }
-        return this;
-    }
-
-    public String getTextSum() {
-        WebElement iframe = driver.findElement(By.cssSelector(".bepaid-iframe"));
-        driver.switchTo().frame(iframe);
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-
-        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(), 'BYN')]")));
-        return element.getText();
-    }
-
-    public String getTextSumButton() {
-        WebElement iframe = driver.findElement(By.cssSelector(".bepaid-iframe"));
-        driver.switchTo().frame(iframe);
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-
-        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button.colored.disabled")));
-        return element.getText();
-    }
-
-    public String getTextPhoneNumber() {
-        WebElement iframe = driver.findElement(By.cssSelector(".bepaid-iframe"));
-        driver.switchTo().frame(iframe);
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-
-        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("p.header__payment-info")));
-        return element.getText();
-    }
-
-    public MtsPage checkFieldsCardDetail(String expectedLabel, String formControlName) {
-        WebElement iframe = driver.findElement(By.cssSelector(".bepaid-iframe"));
-        driver.switchTo().frame(iframe);
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-        WebElement inputField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@formcontrolname='" + formControlName + "']" + "/following-sibling::label")));
-
-        assertTrue(inputField.getText().contains(expectedLabel));
-        return this;
-    }
-
-    public MtsPage checkIconsVisibility() {
-        WebElement iframe = driver.findElement(By.cssSelector(".bepaid-iframe"));
-        driver.switchTo().frame(iframe);
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-        WebElement cardsBrandsContainer = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("cards-brands__container")));
-        List<WebElement> paymentIcons = cardsBrandsContainer.findElements(By.tagName("img"));
-
-        Assertions.assertEquals(5, paymentIcons.size());
-
-        for (WebElement icon : paymentIcons) {
-            wait.until(ExpectedConditions.visibilityOf(icon));
-            Assertions.assertTrue(icon.isDisplayed());
-        }
-        return this;
-    }
-
-    public MtsPage switchToDefaultContent() {
+    public void switchToDefaultContent() {
         driver.switchTo().defaultContent();
-        return this;
     }
 }
-
